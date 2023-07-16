88e7ab160301bd00f12f1d9024805041731ec149
eolender <hyouhakusha@gmail.com>
2023-07-16 22:54:10 +0200

    Add fetching data with axios from backend

--- frontend/src/pages/timelinesListPage/TimelineList.tsx
+++ frontend/src/pages/timelinesListPage/TimelineList.tsx
@@ -1,25 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Timeline } from '../../types/types'
 import styled from 'styled-components';
+import axios from 'axios';
 
 const ListContainer = styled.div`
-background-color: ${(p) => p.theme.colors.background};
-  display: flex;
-  flex-direction: column;
+background-color: ${(p) => p.theme.colors['background']};
+padding: 1rem;
+display: flex;
+flex-direction: column;
 `;
 
 const TimelineItem = styled.div`
-  background-color: ${p => p.theme.colors['accent']};
-  padding: 20px;
-  margin-bottom: 20px;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+display: flex;
+align-items: center;
+padding: 0.5rem;
+background-color: ${(props) => props.theme.colors.accent};
+border-radius: 5px;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+margin: 0.5rem 0; 
 `;
 
 const TimelineTitle = styled.h2`
   color: ${(p) => p.theme.colors['text']};
-  font-size: 20px;
-  margin-bottom: 10px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-right: 1rem;
 `;
 
 const stubTimelines: Timeline[] = [
@@ -47,25 +52,39 @@ const stubTimelines: Timeline[] = [
 
 export const TimelineList = () => {
     const [timeline, setTimeline] = useState<Timeline[]>(stubTimelines)
-// todo: 
-// ostylowanie listy + wydzielenie elementu listy do komponentu własnego - obiekt w liście
-// fetchowanie z backendu rzeczywistych danych,
-// (czy nie daj się ustawić danych w h2 przy starcie aplikacji?) 
-// routing
-// widok pojedyńczego timelinu 
+
+
+    useEffect(() => {
+        fetchTimelines();
+    }, []);
+
+    const fetchTimelines = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/api/timelines');
+            setTimeline(response.data);
+        } catch (error) {
+            console.error('Error fetching timelines:', error);
+        }
+    };
+    // todo: 
+    // ostylowanie listy + wydzielenie elementu listy do komponentu własnego - obiekt w liście
+    // fetchowanie z backendu rzeczywistych danych,
+    // (czy nie daj się ustawić danych w h2 przy starcie aplikacji?) 
+    // routing
+    // widok pojedyńczego timelinu 
     return (
         <div>
-        <h1>TimelineList</h1>
-        <ListContainer>
-    {
-        timeline.map(timeline => (
-            <TimelineItem>
-                <TimelineTitle>{timeline.title}</TimelineTitle> 
-            </TimelineItem>
-        ))
-    }
-        </ListContainer>
-    </div>
-  )
+            <h1>TimelineList</h1>
+            <ListContainer>
+                {
+                    timeline.map(timeline => (
+                        <TimelineItem>
+                            <TimelineTitle>{timeline.title}</TimelineTitle>
+                        </TimelineItem>
+                    ))
+                }
+            </ListContainer>
+        </div>
+    )
 }
 
--- frontend/src/styles.ts
+++ frontend/src/styles.ts
@@ -3,11 +3,11 @@ import { DefaultTheme } from 'styled-components';
 const theme: DefaultTheme = {
     colors: {
     primary: '#816e94',
-    background: '#eabda8',
+    background: '#f6e4dc',
     accent: '#a0c1b9',
     highlight: '#44001a',
     callToAction: '#9a031e',
-    text: '#101312'
+    text: '#101312',
   },
 };
 
