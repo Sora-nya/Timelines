c6b7afe7728b737e93f90fc2cc31122ad71a3251
eolender <hyouhakusha@gmail.com>
2023-07-17 16:33:57 +0200

    Add routing to notesPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Timeline } from './Timeline';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { TimelineList } from './pages/timelinesListPage/TimelineList';
 import theme from './styles';
+import { TimelineList } from './pages/timelinesListPage/TimelineList';
+import { NotesPage } from './pages/notesPage/NotesPage';
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <TimelineList/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TimelineList />} />
+          <Route path="/timeline/:timelineId/notes" element={<NotesPage />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
-    // equivalent to <Timeline></Timeline>
   );
 }
 
--- frontend/src/pages/timelinesListPage/TimelineList.tsx
+++ frontend/src/pages/timelinesListPage/TimelineList.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { Timeline } from '../../types/types'
 import styled from 'styled-components';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const ListContainer = styled.div`
-background-color: ${(p) => p.theme.colors['background']};
+background-color: ${p => p.theme.colors.background};
 padding: 1rem;
 display: flex;
 flex-direction: column;
@@ -14,22 +15,26 @@ const TimelineItem = styled.div`
 display: flex;
 align-items: center;
 padding: 0.5rem;
-background-color: ${(props) => props.theme.colors.accent};
+background-color: ${(p) => p.theme.colors.accent};
 border-radius: 5px;
 box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 margin: 0.5rem 0; 
 `;
 
 const TimelineTitle = styled.h2`
-  color: ${(p) => p.theme.colors['text']};
+  color: ${(p) => p.theme.colors.text};
   font-size: 1.2rem;
   font-weight: bold;
   margin-right: 1rem;
+  text-decoration: none;
+`;
+
+const TimelineLink = styled(Link)`
+  text-decoration: none; /* Remove the text decoration for the Link component */
 `;
 
 export const TimelineList = () => {
     const [timeline, setTimeline] = useState<Timeline[]>()
-
 
     useEffect(() => {
         fetchTimelines();
@@ -45,19 +50,18 @@ export const TimelineList = () => {
     };
     // todo: 
     // ostylowanie listy + wydzielenie elementu listy do komponentu własnego - obiekt w liście +
-    // fetchowanie z backendu rzeczywistych danych, +
-    // (czy nie daj się ustawić danych w h2 przy starcie aplikacji?) 
-    // routing
     // widok pojedyńczego timelinu 
     return (
         <div>
             <h1>TimelineList</h1>
             <ListContainer>
-                {//niech mi Jacek wwytłumaczy czemu ten pytajnik działa.
+                {
                     timeline?.map(timeline => (
-                        <TimelineItem key={timeline.id}>
-                            <TimelineTitle>{timeline.title}</TimelineTitle>
-                        </TimelineItem>
+                        <TimelineLink to={`/timeline/${timeline.id}/notes`}  key={timeline.id}>
+                            <TimelineItem>
+                                <TimelineTitle>{timeline.title}</TimelineTitle>
+                            </TimelineItem>
+                        </TimelineLink>
                     ))
                 }
             </ListContainer>
